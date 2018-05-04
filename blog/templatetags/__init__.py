9338f6019355f3b8a6d3c8774363aedc55ec2dc5
mingjieH <38781826+mingjieH@users.noreply.github.com>
2018-05-04 13:44:11 +0800

    Update __init__.py

--- blog/templatetags/__init__.py
+++ blog/templatetags/__init__.py
@@ -14,8 +14,11 @@ __author__ = 'JHao'
 
 import sys
 
-reload(sys)
-sys.setdefaultencoding('utf-8')
+# reload(sys)
+# sys.setdefaultencoding('utf-8')
+import importlib
+
+importlib.reload(sys)
 
 if __name__ == '__main__':
     pass
