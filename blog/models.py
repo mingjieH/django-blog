f3303fee63bb4967da44e1a6332aa042095a6c9c
mingjieH <hmj279082575@163.com>
2018-06-04 09:07:19 +0800

    修改为个人风格

--- blog/models.py
+++ blog/models.py
@@ -18,23 +18,28 @@ from django.conf import settings
 # Create your models here.
 
 class Tag(models.Model):
-    tag_name = models.CharField(u'标签名', max_length=30)
+    tag_name = models.CharField(verbose_name='标签名', max_length=30)
 
     def __unicode__(self):
         return self.tag_name
 
+    class Meta:
+        ordering = ['tag_name']
+        verbose_name = "标签"
+        verbose_name_plural = verbose_name
+
 
 class Article(models.Model):
-    title = models.CharField(max_length=200)  # 博客标题
-    category = models.ForeignKey('Category', verbose_name=u'文章类型', on_delete=models.CASCADE)
-    date_time = models.DateField(auto_now_add=True)  # 博客日期
-    content = models.TextField(blank=True, null=True)  # 文章正文
-    digest = models.TextField(blank=True, null=True)  # 文章摘要
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name=u'作者', on_delete=models.CASCADE)
-    view = models.BigIntegerField(default=0)  # 阅读数
-    comment = models.BigIntegerField(default=0)  # 评论数
-    picture = models.CharField(max_length=200)  # 标题图片地址
-    tag = models.ManyToManyField(Tag)  # 标签
+    title = models.CharField(verbose_name='标题',max_length=200)  # 博客标题
+    category = models.ForeignKey('Category', verbose_name='类型', on_delete=models.CASCADE)
+    date_time = models.DateField(verbose_name='创建时间',auto_now_add=True)  # 博客日期
+    content = models.TextField(verbose_name='正文')  # 文章正文
+    digest = models.TextField(verbose_name='摘要')  # 文章摘要
+    author = models.ForeignKey(settings.AUTH_USER_MODEL, verbose_name='作者', on_delete=models.CASCADE)
+    view = models.BigIntegerField(verbose_name='阅读数',default=0)  # 阅读数
+    comment = models.BigIntegerField(verbose_name='评论数',default=0)  # 评论数
+    picture = models.CharField(verbose_name='标题图片',upload_to='uploadimg')  # 标题图片地址
+    tag = models.ManyToManyField(Tag,verbose_name='标签')  # 标签
 
     def __unicode__(self):
         return self.title
@@ -60,17 +65,20 @@ class Article(models.Model):
         self.save(update_fields=['comment'])
 
     class Meta:  # 按时间降序
+
         ordering = ['-date_time']
+        verbose_name = "博客"
+        verbose_name_plural = verbose_name
 
 
 class Category(models.Model):
-    name = models.CharField(u'文章类型', max_length=30)
-    created_time = models.DateTimeField(u'创建时间', auto_now_add=True)
-    last_mod_time = models.DateTimeField(u'修改时间', auto_now=True)
+    name = models.CharField(verbose_name='类型', max_length=30)
+    created_time = models.DateTimeField(verbose_name='创建时间', auto_now_add=True)
+    last_mod_time = models.DateTimeField(verbose_name='修改时间', auto_now=True)
 
     class Meta:
         ordering = ['name']
-        verbose_name = u"文章类型"
+        verbose_name = "类型"
         verbose_name_plural = verbose_name
 
     def __unicode__(self):
@@ -78,9 +86,9 @@ class Category(models.Model):
 
 
 class Comment(models.Model):
-    title = models.CharField(u"标题", max_length=100)
-    source_id = models.CharField(u'文章id或source名称', max_length=25)
-    create_time = models.DateTimeField(u'评论时间', auto_now=True)
-    user_name = models.CharField(u'评论用户', max_length=25)
-    url = models.CharField(u'链接', max_length=100)
-    comment = models.CharField(u'评论内容', max_length=500)
+    title = models.CharField(verbose_name="标题", max_length=100)
+    source_id = models.CharField(verbose_name='文章id或source名称', max_length=25)
+    create_time = models.DateTimeField(verbose_name='评论时间', auto_now=True)
+    user_name = models.CharField(verbose_name='评论用户', max_length=25)
+    url = models.CharField(verbose_name='链接', max_length=100)
+    comment = models.CharField(verbose_name='评论内容', max_length=500)
--- django_blog/settings.py
+++ django_blog/settings.py
@@ -133,3 +133,5 @@ STATICFILES_DIRS = (
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
 )
+# MEDIA_ROOT = '/static/'
+# MEDIA_URL = '/static/'
